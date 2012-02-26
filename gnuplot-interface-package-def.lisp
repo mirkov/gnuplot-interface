a830b77b7acba050d40a96bba3d431157b13db47
Mirko Vukovic <mirko.vukovic@gmail.com>
2012-02-25 23:08:10 -0500

    Adding package-definition file
    In previous two commits I mistakenly missed it

---
+++ gnuplot-interface-package-def.lisp
@@ -0,0 +1,39 @@
+;;;; package.lisp
+
+(defpackage #:gnuplot-interface
+  (:nicknames #:gpi)
+  (:use #:cl)
+  (:import-from :alexandria
+		:symbolicate)
+  (:export
+   ;; Many exported commands have a second name to prevent conflicts
+   ;; with other packages.  Thus `command' can also be called with
+   ;; `gnuplot-command'
+   ;;
+   ;; In the lines below, if a line has two symbols, the second symbol
+   ;; is alternate command name
+   :start-gnuplot
+   :init-gnuplot
+   :hello-world :gnuplot-hello-world
+   :test :gnuplot-test
+   :reset :gnuplot-reset 
+   :stop-gnuplot
+   :command :gnuplot-command
+   :echo-command :gnuplot-echo-command
+	   
+   :send-line :send-line-to-gnuplot
+   :send-line-break :send-line-break-to-gnuplot)
+  (:documentation
+"Package for starting a gnuplot subprocess and sending gnuplot
+commands to it.  This package does not introduce plotting commands of
+its own.  It only serves to send command strings to the gnuplot
+process.
+
+The package symbols are not meant to be imported into user packaes. 
+They are  intended to be used as:
+gpi:start-gnuplot
+gp1:command
+gpi:test
+gpi:...
+
+"))
